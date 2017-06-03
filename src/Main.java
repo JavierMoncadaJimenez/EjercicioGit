c6088ebcf749ad070bf7f6c90b39d4deea14761c
Javier <Javier@192.168.37.1>
2017-06-03 13:07:48 +0200

    Se ha insertado dos lineas de salida por consola. Una es una suma

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public Main() {
 		// TODO Auto-generated constructor stub
 		System.out.println("Hola mundo");
+		System.out.println("Mensaje de prueba, diferente al del primer cliente");
+		System.out.println("1 + 1 = 2");
 	}
 
 }
